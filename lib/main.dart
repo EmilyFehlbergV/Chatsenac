17b66a4cab6ee65861a243b7778ead340a170046
Emily Fehlberg Visintin <fehlviimusics@gmail.com>
2026-07-27 21:34:21 -0300

    finaliza a tela de login
    - cria espaçamento entre as bordas do aparelho e botões,adiciona cores aos botões e adiciona icone à eles

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(backgroundColor: Colors.white, body: Builder(
-        builder: (context) {
-          return Login();
-        }
-      )),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Builder(
+          builder: (context) {
+            return SafeArea(child: Login());
+          },
+        ),
+      ),
     );
   }
 }
@@ -21,35 +25,134 @@ class MyApp extends StatelessWidget {
 class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return SingleChildScrollView(
+
+        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+        child: Column( //child column?😭☠️☠️☠️☠️☠️
       crossAxisAlignment: CrossAxisAlignment.stretch,
+      spacing: 16,
       children: [
-        Row(children: [FlutterLogo(size: 18), Text("ChatSENAC") ]),
+        Row(children: [FlutterLogo(size: 18), const SizedBox(width: 8),
+        Text("ChatSENAC")]),
+        SizedBox(height: 32),
+
         //Titulos
-        Text("Entre na sua conta",
-          style: Tipografia.h1,
-           ),
-        Text("Coloque seu email e senha para logar"),
+        Text("Entre na sua conta", style: Tipografia.h1),
+        Text("Coloque seu email e senha para logar", style: Tipografia.subtitulo,),
+        const SizedBox(height: 12),
         //campos
-        Text("Email", style: Tipografia.subtitulo   ),
+        Text("Email", style: Tipografia.subtitulo),
+        const SizedBox(height: 2,),
         TextField(),
-        Text("Senha",style: Tipografia.subtitulo),
-        TextField(),
-        InkWell(child: Text("Esqueceu a senha?",
-          textAlign: TextAlign.right,
-          style: Tipografia.Link,
-        )),
-        ElevatedButton(onPressed: null, child: Text("Entrar")),
-        Text("ou", textAlign: TextAlign.center,),
-        ElevatedButton(onPressed: null, child: Text("Continuar com Google")),
-        ElevatedButton(onPressed: null, child: Text("Continuar com Facebook")),
+
+        const SizedBox(height: 16),
+
+        Text("Senha", style: Tipografia.subtitulo),
+        const SizedBox(height: 2,),
+        TextField(obscureText: true),
+        const SizedBox(height: 16),
+
+        InkWell(
+          child: Text(
+            "Esqueceu a senha?",
+            textAlign: TextAlign.right,
+            style: Tipografia.Link,
+
+          ),
+        ),
+        const SizedBox(height: 5),
+
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+          child: const Text("Entrar"),
+        ),
+        const SizedBox(height: 4,),
+
+        Row(
+          children: [
+            Expanded(child: Divider()),
+            Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text("ou"),
+            ),
+            Expanded(child: Divider())
+
+          ],
+        ),
+
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+
+            padding: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 24.0,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(9),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              const Icon(Icons.g_mobiledata, size: 18),
+
+              const SizedBox(width: 10),
+
+              const Text("Continuar com Google"),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 12),
+
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+
+            padding: const EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 24.0,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(9),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              const Icon(Icons.facebook, size: 18, color: Colors.blue),
+
+              const SizedBox(width: 10),
+
+              const Text("Continuar com Facebook"),
+            ],
+          ),
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("Não tem uma conta?", textAlign: TextAlign.center,),
-            InkWell(child: Text("Cadastre-se", textAlign: TextAlign.center)),],
-        )
+          children: [
+            Text("Não tem uma conta?", textAlign: TextAlign.center),
+            SizedBox(width: 5,),
+            InkWell(child: Text("Cadastre-se",
 
+                textAlign: TextAlign.center,
+                style: Tipografia.Link)),
+
+          ],
+        ),
       ],
+    )
     );
   }
 }
